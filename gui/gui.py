1b6d40c4711dfe8a75f956bd75001470d4895def
Alexander Davenport <alexhdavenport@gmail.com>
2018-01-27 20:32:20 -0500

    numerical fields implemented

--- gui/gui.py
+++ gui/gui.py
@@ -19,6 +19,12 @@ ser.readline()
 #window title
 run_name = "MASA Live Data Dashboard"
 
+#global vars
+launchAlt = 0;
+alt = 0;
+packetsLost = 0;
+last_packet = 0;
+
 #initialize Qt
 app = QtGui.QApplication([])
 #timer
@@ -33,10 +39,6 @@ app.setWindowIcon(QtGui.QIcon('logos/logo.png'))
 layout = QtGui.QGridLayout()
 w.setLayout(layout)
 
-# Zero Indexes for the gui layout (row, column) (sternie's idea)
-zr = 0
-zc = 0
-
 #load graph settings
 graph_settings = pd.read_csv('graph_settings.csv')
 
@@ -45,19 +47,102 @@ def tickCalc(tr, s):
     return int(s/(tr/1000))
 
 #max number of datapoints to store/retrieve
-tick_rate = 100 #in ms (calculated limit at about 35-40 ms)
+tick_rate = 150 #in ms (calculated limit at about 35-40 ms)
 seconds_to_store = graph_settings['seconds'].max() #save as much memory as possible (keep only what's needed)
 data_range = tickCalc(tick_rate, seconds_to_store) #this isn't right and I don't know why
 #last_time = pg.ptime.time()
+data = []
 
 #initialize data arrays (for testing only)
 #eventually load data from web database into dataframe
-cols = ['time','packetType', 'packetNumber', 'lat', 'long', 'baro', 'maxAlt', 'gyroZ', 'accelVel', 'accelZ', 'rssi']
+cols = ['time','packetType', 'packetNumber', 'lat', 'long', 'baro', 'maxAlt', 'gyroZ', 'velAccel', 'accelZ', 'rssi']
 database = pd.DataFrame(columns=cols)
 
 #add area for tiled plots
 plot_box = pg.GraphicsLayoutWidget()
-layout.addWidget(plot_box, zr+0, zc+0)
+plot_box.setBackground(None)
+layout.addWidget(plot_box, 0, 1)
+
+#add data area on side
+data_fields = QtGui.QWidget()
+data_layout = QtGui.QVBoxLayout()
+layout.addWidget(data_fields, 0, 0)
+data_fields.setLayout(data_layout)
+data_layout.addStretch(1)
+
+#comm stats box (add to comm_layout)
+comm_stats = QtGui.QGroupBox("Comm Stats")
+data_layout.addWidget(comm_stats)
+comm_layout = QtGui.QGridLayout()
+comm_stats.setLayout(comm_layout)
+
+comm_layout.addWidget(QtGui.QLabel("Packets Lost: "), 0, 0)
+comm_layout.addWidget(QtGui.QLabel("RSSI: "), 1, 0)
+packetLossLabel = QtGui.QLabel("0")
+rssiLabel = QtGui.QLabel()
+comm_layout.addWidget(packetLossLabel, 0, 1)
+comm_layout.addWidget(rssiLabel, 1, 1)
+
+def reset_comms():
+    global packetsLost
+    packetsLost = 0
+
+resetCommStats = QtGui.QPushButton("Reset")
+resetCommStats.clicked.connect(reset_comms)
+comm_layout.addWidget(resetCommStats, 2, 0)
+
+#flight stats box (add to flight_layout)
+flight_stats = QtGui.QGroupBox("Flight Stats")
+data_layout.addWidget(flight_stats)
+flight_layout = QtGui.QGridLayout()
+flight_stats.setLayout(flight_layout)
+
+flight_layout.addWidget(QtGui.QLabel("Altitude: "), 0, 0)
+flight_layout.addWidget(QtGui.QLabel("Max Altitude: "), 1, 0)
+flight_layout.addWidget(QtGui.QLabel("Velocity: "), 2, 0)
+flight_layout.addWidget(QtGui.QLabel("Acceleration: "), 3, 0)
+flight_layout.addWidget(QtGui.QLabel("Roll: "), 4, 0)
+baroLabel = QtGui.QLabel()
+maxAltLabel = QtGui.QLabel()
+velAccelLabel = QtGui.QLabel()
+accelZLabel = QtGui.QLabel()
+gyroZLabel = QtGui.QLabel()
+flight_layout.addWidget(baroLabel, 0, 1)
+flight_layout.addWidget(maxAltLabel, 1, 1)
+flight_layout.addWidget(velAccelLabel,2 , 1)
+flight_layout.addWidget(accelZLabel, 3, 1)
+flight_layout.addWidget(gyroZLabel, 4, 1)
+flight_layout.addWidget(QtGui.QLabel("ft (ASL)"), 0, 2)
+flight_layout.addWidget(QtGui.QLabel("ft (ASL)"), 1, 2)
+flight_layout.addWidget(QtGui.QLabel("ft/s"), 2, 2)
+flight_layout.addWidget(QtGui.QLabel("ft/s^2"), 3, 2)
+flight_layout.addWidget(QtGui.QLabel("deg/s"), 4, 2)
+
+def zero_altitude():
+    global alt, launchAlt
+    launchAlt = alt
+
+zeroAlt = QtGui.QPushButton("Zero Altitude")
+zeroAlt.clicked.connect(zero_altitude)
+flight_layout.addWidget(zeroAlt, 5, 0)
+
+#location box (add to location_layout)
+location_stats = QtGui.QGroupBox("Location")
+data_layout.addWidget(location_stats)
+location_layout = QtGui.QGridLayout()
+location_stats.setLayout(location_layout)
+
+location_layout.addWidget(QtGui.QLabel("Latitude: "), 0, 0)
+location_layout.addWidget(QtGui.QLabel("Longitude: "), 1, 0)
+latLabel = QtGui.QLabel()
+longLabel = QtGui.QLabel()
+location_layout.addWidget(latLabel, 0, 1)
+location_layout.addWidget(longLabel, 1, 1)
+location_layout.addWidget(QtGui.QLabel("deg"), 0, 2)
+location_layout.addWidget(QtGui.QLabel("deg"), 1, 2)
+
+#center data
+data_layout.addStretch(1)
 
 #status = QtGui.statusBar(top)
 mainMenu = top.menuBar()
@@ -119,7 +204,7 @@ for i in range(len(graph_settings.index)):
 
 #update function runs on each tick
 def update():
-    global database, cols, last_time, ser
+    global database, cols, last_time, ser, alt, packetsLost, last_packet
 
     #print(str((pg.ptime.time()-last_time)*1000-tick_rate) + " ms lag time this tick")
     #last_time = pg.ptime.time()
@@ -167,7 +252,7 @@ def update():
         data.append(struct.unpack("<h", byte_rep)[0]) #gyroZ
 
         byte_rep = packet[17:19]
-        data.append(struct.unpack("<h", byte_rep)[0]) #accelVel
+        data.append(struct.unpack("<h", byte_rep)[0]) #velAccel
 
         byte_rep = packet[19:22]
         accelZ = bin(struct.unpack('<I', byte_rep + b'\xFF')[0])[2:]
@@ -175,8 +260,21 @@ def update():
 
         byte_rep = packet[22:23]
         data.append(struct.unpack("<B", byte_rep)[0])
+        #print(last_packet)
+        if (last_packet != (data[2] - 1)) and (last_packet != -1) and (last_packet != (2^16 - 1)):
+            packetsLost += data[2] - last_packet
 
-        #print(data)
+        last_packet = data[2]
+        packetLossLabel.setText(str(packetsLost))
+        rssiLabel.setText(str(data[10]))
+        baroLabel.setText(str(data[5]-launchAlt))
+        alt = data[5]
+        maxAltLabel.setText(str(data[6]-launchAlt))
+        velAccelLabel.setText(str(data[8]))
+        accelZLabel.setText(str(data[9]))
+        gyroZLabel.setText(str(data[7]))
+        latLabel.setText('%.6f' % data[3])
+        longLabel.setText('%.6f' % data[4])
 
         #update database
         database = database.append(pd.DataFrame([data],columns=cols))
@@ -187,7 +285,6 @@ def update():
         #update plots with new data
         for p in plots:
             p.updatePlot(database)
-
 
 #display window
 #using .showMaximized() instead of .show() until I can figure out sizing
