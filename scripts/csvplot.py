d6344bbf740c2f7a62a01d7aab6ea45694886d7a
nsternie <nsternie@umich.edu>
2018-03-03 21:13:27 -0500

    back

--- scripts/csvplot.py
+++ scripts/csvplot.py
@@ -3,5 +3,28 @@ from matplotlib.backends.backend_pdf import PdfPages
 import pandas as pd
 import sys
 
-datafile = sys.argv[1]
-pdf = PdfPages(data_file_name.rstrip('.csv')+'plots.pdf')
+df = pd.read_csv(sys.argv[1])
+pdf = PdfPages(sys.argv[1].rstrip('.csv')+'plots.pdf')
+
+plot_num = 1
+for col in df:
+    data = df[col]
+
+    # Global data plot
+    plt.figure(0)
+    plt.plot(data)
+
+    # Column data
+    plt.figure(plot_num)
+    plot_num = plot_num + 1
+    plt.plot(data)
+    plt.title(data.name)
+
+plt.figure(0)
+plt.title("ALL DATA")
+
+for n in range(plot_num):
+    plt.figure(n)
+    plt.savefig(pdf, format='pdf')
+
+pdf.close()
