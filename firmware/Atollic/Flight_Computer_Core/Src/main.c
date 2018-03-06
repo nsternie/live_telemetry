9fcf413e1f66a24b181d73201d9c380e31cd8146
jzsig <jzsig@aol.com>
2018-03-05 21:13:11 -0500

    Changed gyros to timer based and added delay on init, which seemed to fix init issues.
    Signed-off-by: jzsig <jzsig@aol.com>

--- firmware/Atollic/Flight_Computer_Core/Src/main.c
+++ firmware/Atollic/Flight_Computer_Core/Src/main.c
@@ -649,7 +649,7 @@ static void MX_TIM10_Init(void)
   htim10.Instance = TIM10;
   htim10.Init.Prescaler = 1799;
   htim10.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim10.Init.Period = 99;
+  htim10.Init.Period = 999;
   htim10.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   if (HAL_TIM_Base_Init(&htim10) != HAL_OK)
   {
